e64e98e688f761d0a148c764b0d71d67f4e196db
GustavoB12 <gustavo.bassoli_BOLS@compasso.com.br>
2021-12-01 15:34:46 -0300

    Initial commit
    Primeiro commit

---
+++ src/test/java/br/com/compass/ControleDeProdutos/ControleDeProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.compass.ControleDeProdutos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleDeProdutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
